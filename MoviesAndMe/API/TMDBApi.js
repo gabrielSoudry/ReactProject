4c04a277b14b043ca7568c340bf43e6124ea7c3c
Gabi22top <gabriel_s@hotmail.fr>
2019-07-26 18:01:40 +0200

    Display movie from the API

--- MoviesAndMe/API/TMDBApi.js
+++ MoviesAndMe/API/TMDBApi.js
@@ -1,7 +1,11 @@
-import API_K from './config'
-    
-const API_TOKEN = API_K;
+import API_K from './config_api'
+const API_TOKEN = API_K
 
 export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text    
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
 }
+
+
--- MoviesAndMe/Components/Search.js
+++ MoviesAndMe/Components/Search.js
@@ -2,8 +2,8 @@
 
 import React from 'react'
 import { Text, StyleSheet, View, TextInput, Button, FlatList } from 'react-native'
-import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+import  {getFilmsFromApiWithSearchedText} from '../API/TMDBApi.js'
 
 const styles = StyleSheet.create({
   textinput: {
@@ -18,18 +18,28 @@ const styles = StyleSheet.create({
 
 class Search extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { films: [] }
+  }
+
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText("star").then(data => {
+        this.setState({ films: data.results })
+    })
+}
+
   render() {
     return (
       <View style={{ marginTop: 40 }}>
 
         <TextInput style={styles.textinput} placeholder='Titre du film' />
-        <Button title='Rechercher' onPress={() => { }} />
+        <Button title='Rechercher' onPress={() => this._loadFilms()} />
 
         <FlatList
-          data={films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => <FilmItem film={item} />}
-
         />
 
       </View>
